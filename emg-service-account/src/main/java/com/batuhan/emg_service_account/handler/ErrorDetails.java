ca2be72e694b239eed50bc36c6e9ddb5cfdbfcc8
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-10 18:55:17 +0300

    Create handler

---
+++ emg-service-account/src/main/java/com/batuhan/emg_service_account/handler/ErrorDetails.java
@@ -0,0 +1,18 @@
+package com.batuhan.emg_service_account.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+    private String errorCode;
+}
