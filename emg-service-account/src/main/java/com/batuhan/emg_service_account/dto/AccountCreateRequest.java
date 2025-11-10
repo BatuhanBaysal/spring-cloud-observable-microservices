b481ee2d971e44f1c004c1c829a1ff75a3b526db
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-10 18:50:04 +0300

    Create AccountCreateRequest.java

---
+++ emg-service-account/src/main/java/com/batuhan/emg_service_account/dto/AccountCreateRequest.java
@@ -0,0 +1,31 @@
+package com.batuhan.emg_service_account.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Data
+public class AccountCreateRequest {
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50, message = "First name cannot exceed 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50, message = "Last name cannot exceed 50 characters")
+    private String lastName;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 4, max = 50, message = "Username must be between 4 and 50 characters")
+    private String username;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    @Size(max = 100, message = "Email cannot exceed 100 characters")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
