8088af3d9969c16ba3762e172915848db23ce084
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-15 13:40:01 +0300

    Create emg-config-server

---
+++ emg-config-server/src/test/java/com/batuhan/emg_config_server/EmgConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batuhan.emg_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmgConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
