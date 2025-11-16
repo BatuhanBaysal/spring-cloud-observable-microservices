7f6d6c343ea8e98d0f0e0e4f7ae8765cd3dd91e9
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-16 11:58:30 +0300

    Create SecurityConfig.java

--- emg-service-product/src/main/java/com/batuhan/emg_service_product/config/SecurityConfig.java
+++ emg-service-product/src/main/java/com/batuhan/emg_service_product/config/SecurityConfig.java
@@ -2,15 +2,17 @@ package com.batuhan.emg_service_product.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final InternalAccessFilter internalAccessFilter;
@@ -25,13 +27,14 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
 
-                .addFilterBefore(internalAccessFilter, LogoutFilter.class)
+                .addFilterBefore(internalAccessFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
 
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**", "/eureka/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
